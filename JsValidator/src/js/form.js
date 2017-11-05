7d18432d492143894202ea92398a0cf7603a7ce5
Unknown <wieczorek.karolina1@o2.pl>
2017-11-05 23:53:18 +0100

    JsVal completed

--- JsValidator/src/js/form.js
+++ JsValidator/src/js/form.js
@@ -2,5 +2,15 @@
  * Created by Jacob on 2017-10-31.
  */
 function validate() {
-    
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var subject = document.getElementById('subject').value;
+    var message = document.getElementById('message').value;
+
+    if (name == '' || email == '' || subject == '' || message == '') {
+        alert('Please fill in the form');
+    }
+    else {
+        document.getElementById('feedback').innerHTML = 'Thank you for filling in the form';
+    }
 }
