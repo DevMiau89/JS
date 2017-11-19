831cb81e35f0d51ef422236bd9ad5ccaa48c7996
Unknown <wieczorek.karolina1@o2.pl>
2017-11-19 23:49:38 +0100

    DrumKit completed

--- Js30daysProjects/DrumKit/src/js/drumKit.js
+++ Js30daysProjects/DrumKit/src/js/drumKit.js
@@ -1,4 +1,27 @@
 /**
  * Created by Jacob on 2017-11-18.
  */
+function playSound(e) {
+    console.log(e.keyCode);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    console.log(audio);
 
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing')
+
+}
+function removeTransition(e) {
+    console.log("dziala");
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+$( document ).ready(function() {
+    const keys = document.querySelectorAll('.key');
+
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+    window.addEventListener('keydown', playSound);
+});
