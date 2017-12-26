84daba0ff281a675b304cc1b5d86e6deef5aa3ac
Unknown <wieczorek.karolina1@o2.pl>
2017-12-26 23:18:52 +0100

    JsVideo initialised

---
+++ Js30daysProjects/JsVideoPlayer/scripts.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Jacob on 2017-12-26.
+ */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
