dd134ab2282163295f26d3a462d7389e162d8f68
Unknown <wieczorek.karolina1@o2.pl>
2018-01-11 23:15:44 +0100

    Js toDoList continued...

---
+++ JsExercises/BasicExercises/projekt-todo/project/js/script.js
@@ -0,0 +1,5 @@
+/* Add element to the list */
+
+const toDoForm = document.querySelector('.todo-form');
+const toDoList = document.querySelector('.todo-list-cnt');
+const toDOSearch = document.querySelector('.todo-list-search-form');
