82c148676a11dffd5f40ba17b7f42d14ae28d568
Unknown <wieczorek.karolina1@o2.pl>
2018-01-19 23:25:27 +0100

    Js leadingZero/convertMS functions added

--- JsExercises/BasicExercises/project-daysToBirthday/project/js/script.js
+++ JsExercises/BasicExercises/project-daysToBirthday/project/js/script.js
@@ -1,3 +1,21 @@
 /**
  * Created by Jacob on 2018-01-18.
  */
+const inputName = document.querySelector('#formName').value;
+const inputDate = document.querySelector('#formDate').value;
+
+
+function leadingZero(i) {
+    return (i < 10)? '0'+i : i;
+}
+
+(function convertMS(){
+    const today = new Date();
+    const date = `${today.getFullYear()} 
+                  ${leadingZero((today.getMonth()+1))} 
+                  ${leadingZero(today.getDate())} 
+                  ${leadingZero(today.getHours())} 
+                  ${leadingZero(today.getMinutes())}`;
+    return date
+
+})();
