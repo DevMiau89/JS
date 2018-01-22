68794adf189d34908686059c8ed9175fc40ef3a0
Unknown <wieczorek.karolina1@o2.pl>
2018-01-22 15:38:37 +0100

    JsdaysToBirthday comopleted

--- JsExercises/BasicExercises/project-daysToBirthday/project/js/script.js
+++ JsExercises/BasicExercises/project-daysToBirthday/project/js/script.js
@@ -1,25 +1,23 @@
 /**
  * Created by Jacob on 2018-01-18.
  */
-const inputName = document.querySelector('#formName').value;
-const inputDate = document.querySelector('#formDate').value;
+const form = document.querySelector('#Form');
+const result = document.querySelector('#result');
 
+function nextBirthday(e) {
+    e.preventDefault();
+        const inputName = document.querySelector('#formName').value;
+        const inputDate = document.querySelector('#formDate').value;
 
-function leadingZero(i) {
-    return (i < 10)? '0'+i : i;
+        const inputBirthday = inputDate.split('.');
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        const month = months[(inputBirthday[1][1]) - 1];
+        const nextBirthdayMS = Date.parse(month + " " + (inputBirthday[0]).toString() + ", " + (new Date().getFullYear()).toString());
+        const todayMS = (new Date()).getTime();
+        const oneDay = 24 * 60 * 60 * 1000;
+
+        const daysToBirthday = (Math.floor((nextBirthdayMS - todayMS) / oneDay)) + 1;
+        result.innerText = `${inputName} there are only ${daysToBirthday} until your Birthday!`;
 }
 
-(function convertMS(){
-    const today = new Date();
-    const oneDay = 24*60*60*1000;
-    const date = today.getTime();
-    return date
-
-})();
-
-(function nextBirthday() {
-    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const nextBirhday = Date.parse(inputBirthday[1] + inputBirthday[0]).toString() + (new Date().getFullYear()).toString();
-    return inputBirthday
-
-})();
+form.addEventListener('submit', nextBirthday);
