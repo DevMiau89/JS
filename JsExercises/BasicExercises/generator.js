3e9b10f9ea9b664b6a409ee1f0bf7350f4bfd0f6
Unknown <wieczorek.karolina1@o2.pl>
2018-02-04 21:13:12 +0100

    gulp autoprefixer

--- JsExercises/BasicExercises/generator.js
+++ JsExercises/BasicExercises/generator.js
@@ -1,6 +1,11 @@
 /**
  * Created by Jacob on 2018-02-03.
  */
+
+// import console from 'console'
+
+// const console = require('console').default;
+
 function* myGenerator() {
     yield 1;
     yield 2;
@@ -8,7 +13,7 @@ function* myGenerator() {
 }
 
 var g = myGenerator();
-console.log(g);
+
 console.log(g.next());
-console.log(g.next());
-console.log(g.next());
+g.next();
+g.next();
--- lj/gulpfile.js
+++ lj/gulpfile.js
@@ -1,13 +1,14 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function(){
     // Run ESLint
     gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
     .pipe(eslint())
     .pipe(eslint.format());
-     Node source
+    // Node source
     gulp.src("es6/**/*.js")
     .pipe(babel())
     .pipe(gulp.dest("dist"));
@@ -15,6 +16,16 @@ gulp.task('default', function(){
     gulp.src("public/es6/**/*.js")
     .pipe(babel())
     .pipe(gulp.dest("public/dist"));
-    
-    console.log('Hello!');
+
 });
+
+
+gulp.task('styles', function () {
+    gulp.src("es6/**/*.css", "public/es6/**/*.css")
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("public/dist"))
+});
+
+gulp.task('watch', function () {
+    gulp.watch("es6/**/*.css", "public/es6/**/*.css", gulp.parallel('css'));
+})
