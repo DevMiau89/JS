cd20d6cb60c7c68814a8398d9f492caa5188a8fb
Unknown <wieczorek.karolina1@o2.pl>
2018-02-12 22:09:29 +0100

    Added Kaden's algorithm

--- JsExercises/BasicExercises/memoize.js
+++ JsExercises/BasicExercises/memoize.js
@@ -14,3 +14,23 @@ var fibonacci = memoize(function(n) {
         return n;
     return fibonacci(n-1) + fibonacci(n-2);
 });
+
+var maxSequence = function(array){
+  // ...
+  let max_so_far = 0;
+  let max_ending = 0;
+
+  for(let i=0; i < array.length; i++) {
+     max_ending = max_ending + array[i];
+     if(max_ending < 0) {
+       max_ending = 0;
+     }
+     else if(max_so_far < max_ending){
+       max_so_far = max_ending;
+     }
+     console.log(max_so_far);
+     console.log(max_ending);
+  }
+
+  return max_so_far;
+}
