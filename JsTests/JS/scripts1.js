bd645a3f3fb5d06634e0f0ca34a48e63d53d8895
Unknown <wieczorek.karolina1@o2.pl>
2018-02-26 00:05:06 +0100

    JsSolve fn

--- JsTests/JS/scripts1.js
+++ JsTests/JS/scripts1.js
@@ -5,3 +5,41 @@ function validate(n){
       return false;
     }    
 }
+
+function solve(arr){
+  //..
+  let sortedArr = arr.sort();
+  let counts = {};
+  let result = [];
+  let res = [];
+  let output = [];
+  for(let i=0; i<sortedArr.length;i++){
+    let num = arr[i];
+    counts[num] = counts[num] ? counts[num] + 1 : 1;    
+  }
+  
+   console.log(counts);
+  
+  for(let num in counts){
+      result.push((num.toString()).repeat(counts[num]));
+  }
+  
+  
+  result.forEach(function(el){
+      res.push(el);
+    
+  });
+  
+  res.sort(function(a,b) {
+    return b.length - a.length || Number(a) - Number(b);
+  });
+  
+  res.forEach(function(el){
+    console.log("El is - " + el);
+    let temp = el.split('').map(a => Number(a));
+    console.log("Temp is - " + temp);
+    output = output.concat(temp);
+  });
+  
+  return output;
+ }
--- JsTests/JS/tests1.js
+++ JsTests/JS/tests1.js
@@ -3,3 +3,9 @@ describe("validate", function(){
         expect(validate(1714)).toEqual(false);
     })
 })
+
+describe("Sort", function(){
+    it("should sort values by decreasing frequency", function(){
+        expect(solve([2,3,5,3,7,9,5,3,7])).toEqual([3,3,3,5,5,7,7,2,9]e);
+    })
+})
