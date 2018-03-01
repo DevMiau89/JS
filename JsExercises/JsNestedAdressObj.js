b12492b38b2ba18559f5ce60051abfe8417eba2b
Unknown <wieczorek.karolina1@o2.pl>
2018-03-01 14:17:52 +0100

    JsNested dateFormat added

--- JsExercises/JsNestedAdressObj.js
+++ JsExercises/JsNestedAdressObj.js
@@ -10,16 +10,10 @@ let addressObj = {
         'fn': function () {
             return this.street + ' ' + this.homeNumber + ' ' + this.postalCode;
         },
-        'isFromState': function (city) {
-            if (city == this.city) {
-                return true;
-            }
-            return false;
-
-
-        },
     },
-
+    'isFromState': function (city) {
+        return (city === this.address.city);
+    },
     'fnAddress': function () {
         return this.address;
     }
@@ -27,4 +21,25 @@ let addressObj = {
 };
 console.log(addressObj.address.fn());
 console.log(addressObj.address.fn());
-console.log(addressObj.address.isFromState('JS'));
+console.log(addressObj.isFromState('JS'));
+
+
+
+
+function zeroAdd(d){
+    if( d < 10){
+        return '0' + d;
+    }
+}
+console.log(zeroAdd(5));
+
+/*Date in dd/mm/yyyy format */
+const formattedDate = function () {
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${zeroAdd(day)}\\${zeroAdd(month)}\\${year}`;
+};
+
+console.log(formattedDate());
