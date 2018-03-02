6d52d31eb7e9c0bbad0a5fde49f38242668ed98d
Unknown <wieczorek.karolina1@o2.pl>
2018-03-02 01:01:59 +0100

    JsFactoryPattern

--- JsExercises/JsNestedAdressObj.js
+++ JsExercises/JsNestedAdressObj.js
@@ -6,13 +6,13 @@ let addressObj = {
         'street': 'Main Street',
         'homeNumber': '2A',
         'postalCode': '64-920',
-        'city': 'JS',
+        1: 'JS',
         'fn': function () {
             return this.street + ' ' + this.homeNumber + ' ' + this.postalCode;
         },
     },
     'isFromState': function (city) {
-        return (city === this.address.city);
+        return (city === this.address[1]);
     },
     'fnAddress': function () {
         return this.address;
@@ -43,3 +43,52 @@ const formattedDate = function () {
 };
 
 console.log(formattedDate());
+/*Dynamic prototype pattern */
+function DynamicProtoPattern(name, age, city) {
+    this.age = age;
+    this.name = name;
+    this.city = city;
+
+    if(typeof this.printPerson !== 'function'){
+        DynamicProtoPattern.prototype.printPerson = function () {
+        console.log(this.city + " " + this.name + " " + this.age);
+        }
+    }
+}
+
+const obj = new DynamicProtoPattern('kot', 22, 'kotowo');
+console.log(obj);
+
+function factorial(n) {
+    if(n === 0){
+        return 1;
+    }else{
+        return n * arguments.callee(n - 1);
+    }
+}
+
+console.log(factorial(4));
+
+function Computer(name, ramMemory, processor){
+    this.name = name;
+    this.ramMemory = ramMemory;
+    this.processor = processor
+}
+
+var computerObj = new Computer('Samsung', '16GB', "Celeron");
+
+console.log(computerObj);
+
+function ObjPattern(name, surname, age, city){
+    var ob = {};
+    ob.name = name;
+    ob.surname = surname;
+    ob.age = age;
+    ob.city = city;
+    return ob;
+}
+
+var pattern = ObjPattern("DevMiau", "Koci", 23, "Kotowo");
+console.log(pattern);
+
+
