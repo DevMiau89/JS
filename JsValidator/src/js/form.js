4589945b5fbd27e3d1d3eca35d7342a8dabfe7e2
Unknown <wieczorek.karolina1@o2.pl>
2018-03-09 15:41:29 +0100

    jsValuePrePopulation

--- JsValidator/src/js/form.js
+++ JsValidator/src/js/form.js
@@ -1,17 +1,36 @@
 /**
  * Created by Jacob on 2017-10-31.
  */
-function validate() {
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var subject = document.getElementById('subject').value;
-    var message = document.getElementById('message').value;
 
-    if (name == '' || email == '' || subject == '' || message == '') {
-        alert('Please fill in the form');
+document.addEventListener('DOMContentLoaded', function () {
+    function validate() {
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+        var subject = document.getElementById('subject').value;
+        var message = document.getElementById('message').value;
+
+        if (name == '' || email == '' || subject == '' || message == '') {
+            alert('Please fill in the form');
+        }
+        else {
+            document.getElementById('feedback').innerHTML = 'Thank you for filling in the form';
+        }
     }
-    else {
-        document.getElementById('feedback').innerHTML = 'Thank you for filling in the form';
-    }
-}
+
+    (function prePopulate(){
+        const formInputs = document.querySelectorAll('#myForm input');
+        formInputs.forEach(function (input) {
+          if(input.id === 'name'){
+              input.setAttribute('placeholder','DevMiau');
+          }
+          else if(input.id === 'email'){
+              console.log(input.email);
+              input.setAttribute('placeholder','koci@koci.com');
+          }else{
+              input.setAttribute('placeholder','Please Enter the subject');
+          }
+        })
+    })();
+});
+
 
