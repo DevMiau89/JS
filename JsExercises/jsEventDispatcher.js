13a9e045862e4fee0fd29aa254441ffc5786eab7
Unknown <wieczorek.karolina1@o2.pl>
2018-03-14 14:40:51 +0100

    JsEventDispatcher

--- JsExercises/jsEventDispatcher.js
+++ JsExercises/jsEventDispatcher.js
@@ -1,3 +1,23 @@
 /**
  * Created by DevMiau on 14-Mar-18.
  */
+(function getSelectionBounds() {
+    var x = 'abc';
+    var y = 'xyz';
+    var event = new CustomEvent('build', {
+        "detail": {"x":x,"y":y }
+    });
+    var elem = document.querySelector('#button');
+
+    elem.addEventListener('build', function (e) {
+        alert(e.detail.x + '  ' + e.detail.y);
+    },false);
+    elem.dispatchEvent(event);
+})();
+
+
+
+
+
+
+
