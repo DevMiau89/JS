9be14007ecb89c1807040d943badb2996632c584
Unknown <wieczorek.karolina1@o2.pl>
2018-03-29 20:00:59 +0200

    JsCelsiusToFarenheit

--- JsExercises2/JsCelsiusToFarenheit.js
+++ JsExercises2/JsCelsiusToFarenheit.js
@@ -1,3 +1,11 @@
 /**
  * Created by DevMiau on 29-Mar-18.
  */
+var constant = 32;
+
+function celsiusToFarenheit(temperature){
+    var farenheitTemp = temperature * 9 / 5 + constant;
+    return farenheitTemp;
+}
+
+document.write(celsiusToFarenheit(100));
