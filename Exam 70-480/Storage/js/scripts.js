8ae89512bc4620d8b705429f2644906ba715f618
Unknown <wieczorek.karolina1@o2.pl>
2018-06-19 13:49:45 +0200

    SaveToLocaleStorage function completed

--- Exam 70-480/Storage/js/scripts.js
+++ Exam 70-480/Storage/js/scripts.js
@@ -1,10 +1,19 @@
+'use strict';
 
 function loadFromLocaleStorage() {
+
     
 }
 
 function saveToLocaleStorage() {
+    let key = document.querySelector("input[name= 'Key']");
+    let value = document.querySelector("input[name= 'Value']");
     
+    let keySavedToStorage = JSON.stringify(key.value);
+    let valueSavedToStorage = JSON.stringify(value.value);
+    localStorage.setItem(keySavedToStorage, valueSavedToStorage);
+    key.value = '';
+    value.value = '';
 }
 
 function clearLocaleStorage() {
@@ -23,7 +32,7 @@ var loadFromLocaleStorage = document.getElementById('loadFromLocaleStorage');
 
 
 addToStorage.addEventListener('click', saveToLocaleStorage);
-removeFromStorage.addEventListener('click', RemoveItemFromStorage);
-clearLocaleStorage.addEventListener('click', clearLocaleStorage);
-loadFromLocaleStorage.addEventListener('click', loadFromLocaleStorage);
+//removeFromStorage.addEventListener('click', RemoveItemFromStorage);
+//clearLocaleStorage.addEventListener('click', clearLocaleStorage);
+//loadFromLocaleStorage.addEventListener('click', loadFromLocaleStorage);
 
