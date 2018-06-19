00306cd94b411608f9ea392774e414d1e29e3991
Unknown <wieczorek.karolina1@o2.pl>
2018-06-19 13:01:15 +0200

    Listeners added

--- Exam 70-480/Storage/js/scripts.js
+++ Exam 70-480/Storage/js/scripts.js
@@ -11,7 +11,7 @@ function clearLocaleStorage() {
     
 }
 
-function RemoveItemFromStorage(){
+function RemoveItemFromStorage() {
     
 }
 
@@ -19,4 +19,11 @@ function RemoveItemFromStorage(){
 var addToStorage = document.getElementById('btnAdd');
 var removeFromStorage = document.getElementById('btnRemove');
 var clearLocaleStorage = document.getElementById('btnClear');
+var loadFromLocaleStorage = document.getElementById('loadFromLocaleStorage');
+
+
+addToStorage.addEventListener('click', saveToLocaleStorage);
+removeFromStorage.addEventListener('click', RemoveItemFromStorage);
+clearLocaleStorage.addEventListener('click', clearLocaleStorage);
+loadFromLocaleStorage.addEventListener('click', loadFromLocaleStorage);
 
