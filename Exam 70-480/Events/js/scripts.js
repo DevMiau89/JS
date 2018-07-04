47850feffe2072b7186a959a0d4cd9004d283434
Unknown <wieczorek.karolina1@o2.pl>
2018-07-04 23:47:36 +0200

    main page

--- Exam 70-480/Events/js/scripts.js
+++ Exam 70-480/Events/js/scripts.js
@@ -1,1 +1,9 @@
-
+window.addEventListener("DOMContentLoaded", function() {
+    const btnOuter = document.getElementById('#btnOne');
+    const btnMiddle = document.getElementById('#btnTwo');
+    const btnInner = document.getElementById('#btnThree');
+    
+    
+    function 
+    
+});
