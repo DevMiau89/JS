abc8956e25c96269e1b9daf63d5ac90cb447cc34
Unknown <wieczorek.karolina1@o2.pl>
2018-08-08 23:44:28 +0200

    debugged code added

--- JsTextEditor/js/scripts.js
+++ JsTextEditor/js/scripts.js
@@ -1,35 +1,35 @@
-//var dataFromServer = [{
-//    "Name": "John Smith",
-//    "Birthday": "\/Date(1330848000000-0800)\/"
-//}, {
-//    "Name": "Jane Smith",
-//    "Birthday": "\/Date(1320825600000-0800)\/"
-//}];
-//
-//var dataFromServer = dataFromServer.map(function (item) {
-//    return {
-//        name: item.Name,
-//        birthday: moment(item.Birthday).toDate()
-//    };
-//});
-//
-//console.log("mapped", dataFromServer);
+var dataFromServer = [{
+    "Name": "John Smith",
+    "Birthday": "\/Date(1330848000000-0800)\/"
+}, {
+    "Name": "Jane Smith",
+    "Birthday": "\/Date(1320825600000-0800)\/"
+}];
 
-//Object.defineProperty(window, "MEANING_OF_LIFE", {
-//  writable: true,
-//  value: 42
-//});
-//
-//console.log(window.MEANING_OF_LIFE);
-//window.MEANING_OF_LIFE = 24;
-//console.log(window.MEANING_OF_LIFE);
-//
-//
-//var easyCombination = [13],
-//  hardCombination   = new Array(42, 16, 21),
-//  combined = easyCombination.concat(hardCombination);
-//
-//console.log(JSON.stringify(combined));
+var dataFromServer = dataFromServer.map(function (item) {
+    return {
+        name: item.Name,
+        birthday: moment(item.Birthday).toDate()
+    };
+});
+
+console.log("mapped", dataFromServer);
+
+Object.defineProperty(window, "MEANING_OF_LIFE", {
+  writable: true,
+  value: 42
+});
+
+console.log(window.MEANING_OF_LIFE);
+window.MEANING_OF_LIFE = 24;
+console.log(window.MEANING_OF_LIFE);
+
+
+var easyCombination = [13],
+  hardCombination   = new Array(42, 16, 21),
+  combined = easyCombination.concat(hardCombination);
+
+console.log(JSON.stringify(combined));
 
 
 var dataFromServer = '{ "name": "John Smith", "phone": [ "555-123-4567", "123-456-7890" ], "age": 28 }';
