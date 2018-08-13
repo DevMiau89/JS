a1a84a66b3aef9945fe156f2299f1a141d5347e2
Unknown <wieczorek.karolina1@o2.pl>
2018-08-14 00:22:00 +0200

    promises and arrays

--- JsTextEditor/js/scripts.js
+++ JsTextEditor/js/scripts.js
@@ -48,39 +48,136 @@
 //console.log(toStr);
 //console.log(parsed);
 
-function* generator(str){
-    var x = str.split('');
-    for(var i=0; i < x.length; i++){
-       yield x[i]; 
-    }
-    
+//var x = 2;
+//
+//function doSomething(success, failure){
+//    if(x === 2){
+//      return success(x);  
+//    }else{
+//      return failure();    
+//    }
+//}
+//
+//function doSomethingElse(res, success, failure){
+//    if(res === 3){
+//      return success(res);  
+//    }else{
+//      return failure();    
+//    }
+//}
+//
+//function doThirdThing(newRes, success, failure){
+//    if(newRes === 2){
+//      return success(newRes);  
+//    }else{
+//      return failure();    
+//    }
+//}
+//
+//function failureCallback(){
+//    console.log('fail');
+//}
+//
+//
+//doSomething(function(result){
+//    doSomethingElse(result,function(newResult){
+//        doThirdThing(newResult, function(finalResult){
+//            console.log('The final result: '+ finalResult)
+//        }, failureCallback);
+//    }, failureCallback);
+//}, failureCallback);
+//
+//
+//var promise1 = new Promise(function(resolve, reject){
+//     if(x === 2){
+//      return resolve(x);  
+//    }else{
+//      return reject();    
+//    }
+//})  
+//
+//var promise2 = new Promise(function(resolve, reject){
+//     if(x === 2){
+//      return resolve(x);  
+//    }else{
+//      return reject();    
+//    }
+//})
+//
+//var promise3 = new Promise(function(resolve, reject){
+//     if(x === 2){
+//      return resolve(x);  
+//    }else{
+//      return reject();    
+//    }
+//})
+//
+//promise1.then(function(result){
+//    return promise2
+//}).then(function(result){
+//    return promise3
+//}).then(function(result){
+//    console.log(result + ' finished')
+//}).catch(function(error){
+//    console.log('Something went wrong');
+//})
+
+
+var unsortedArray = [-10, 7, 29, 30, 5, -10, -70];
+
+function findLargest(arr){
+    arr.sort(function(a,b){
+        return a - b;   
+    })   
+    return arr;
+}
+//console.log(findLargest(unsortedArray));
+
+var arrayOfIntegers = [2, 5, 1, 4, 9, 6, 3, 7];
+
+var sorted = findLargest(arrayOfIntegers);
+
+console.log(sorted)
+
+for(var i=0; i < sorted.length-1; i++){
+    if(sorted[i + 1] !== sorted[i] + 1){
+           console.log(sorted[i] + 1);
+       }
 }
 
-var n = generator("yolo");
-console.log(n.next());
-console.log(n.next());
-console.log(n.next());
-console.log(n.next());
-console.log(n.next());
-
-function generator(input){
-	var i = 0;	
-	return {
-		next: function(){
-            var arr = input.split('');
-            
-            if(i < arr.length){
-                i++;
-                return arr[i-1];
-			return "";	
-		}
-		
-		
-	}
-	
-  }
-}
-
-var myGen = generator('boom');
-console.log(myGen.next());
-console.log(myGen.next());
+//function* generator(str){
+//    var x = str.split('');
+//    for(var i=0; i < x.length; i++){
+//       yield x[i]; 
+//    }
+//    
+//}
+//
+//var n = generator("yolo");
+//console.log(n.next());
+//console.log(n.next());
+//console.log(n.next());
+//console.log(n.next());
+//console.log(n.next());
+//
+//function generator(input){
+//	var i = 0;	
+//	return {
+//		next: function(){
+//            var arr = input.split('');
+//            
+//            if(i < arr.length){
+//                i++;
+//                return arr[i-1];
+//			return "";	
+//		}
+//		
+//		
+//	}
+//	
+//  }
+//}
+//
+//var myGen = generator('boom');
+//console.log(myGen.next());
+//console.log(myGen.next());
