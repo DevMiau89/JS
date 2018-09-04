b03fb70927e12bf6c96cf032557cc7dabee26582
Unknown <wieczorek.karolina1@o2.pl>
2018-09-04 23:31:46 +0200

    data plotted to chart

--- junior-js-challenge-master/static/scripts.js
+++ junior-js-challenge-master/static/scripts.js
@@ -26,65 +26,39 @@ const ctx = canvas.getContext('2d')
 
 const btn = document.querySelector('#btn');
 
+function leadingZero(i) {
+    return (i < 10)? '0'+i : i;
+}
 
-let stockName = '';
+function timeConverter(UNIX_timestamp){
+  var a = new Date(UNIX_timestamp);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+  var month = leadingZero(a.getMonth() + 1);
+  var date = leadingZero(a.getDate());
+  var hour = leadingZero(a.getHours());
+  var min = leadingZero(a.getMinutes())
+  var sec = leadingZero(a.getSeconds());
+  var time = date + '.' + month + '.' + year + ' ' + hour + ':' + min + ':' + sec ;
+  return time;
+}
+
+var timeArray = [1536033607702, 1536037207702, 1536040807702, 1536044407702, 1536048007702, 1536051607702, 1536055207702, 1536058807702, 1536062407702, 1536066007702]
+var labelArray = timeArray.map(element => timeConverter(element))
+
+let stockName;
 let arrayOfStocks = [];
-fetch('./stocks').then(resp => resp.json()).then(resp => {(Array.from(resp.stockSymbols)).forEach(function(el){        
-        var divWithStocks =  document.querySelector('#Stocks'); 
-        var pWithoutText = document.createElement('p');
-        pWithoutText.textContent = el;
-        divWithStocks.appendChild(pWithoutText);   
-        stockName = el;
-        arrayOfStocks.push(el)
-    });    
-})
 
-document.getElementById('Stocks').addEventListener('click', function(e){
-        if(e.target && e.target.nodeName == 'P') {
-            fetch(`./stocks/${stockName}`).then(resp => resp.json()).then(resp => {console.log(resp)})                 
-        }    
-}) 
-
-document.addEventListener('DOMContentLoaded', function(){
-        const spinner = document.querySelector('span.spinner');
-        spinner.style.display = 'none';
-});
-
-
-let chart = new Chart(canvas, {
+var chart = new Chart(canvas, {
     type: 'line',
     data: {
-        labels: arrayOfStocks,
-        datasets : [{
-            label : 'MSFT',
-            borderColor: "#3e95cd",
-            fill: false
-        },
-                   {
-            label : 'AAPL',
-            borderColor: "#8e5ea2",
-            fill: false
-        },
-        {
-            label : 'FB',
-            borderColor: "#3cba9f",
-            fill: false
-        },
-        {
-            label : 'EA',
-            borderColor: "#e8c3b9",
-            fill: false
-        },
-        {
-            label : 'IBM',
-            borderColor: "#c45850",
-            fill: false
-        }]
+        labels: labelArray,
+        datasets : []
     },
     options: {
       title: {
         display: true,
-        text: 'Predicted world population (millions) in 2050'
+        text: 'Stocks'
       }, 
         scales: {
         yAxes: [{ 
@@ -102,3 +76,38 @@ let chart = new Chart(canvas, {
       }
     }
 }) 
+
+fetch('./stocks').then(resp => resp.json()).then(resp => {(Array.from(resp.stockSymbols)).forEach(function(el){        
+        var divWithStocks =  document.querySelector('#Stocks'); 
+        var pWithoutText = document.createElement('p');
+        pWithoutText.textContent = el;
+        divWithStocks.appendChild(pWithoutText);   
+        stockName = el;
+        arrayOfStocks.push(el);
+        fetch(`./stocks/${el}`).then(resp => resp.json()).then(resp => {dataSet = {
+            label: el,
+            data: resp.map(x => x.value),
+            fill: false
+        }; chart.data.datasets.push(dataSet); chart.data.labels = resp.map(y => timeConverter(y.timestamp)); chart.update();});
+                                                                       
+    });    
+})
+
+
+
+document.getElementById('Stocks').addEventListener('click', function(e){
+        if(e.target && e.target.nodeName == 'P') { 
+            fetch(`./stocks/${stockName}`).then(resp => resp.json()).then(resp =>console.log(resp))  
+
+        }    
+}) 
+
+document.addEventListener('DOMContentLoaded', function(){
+        const spinner = document.querySelector('span.spinner');
+        spinner.style.display = 'none';
+
+});
+
+
+
+
