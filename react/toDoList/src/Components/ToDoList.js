2983f1a0528e3cff0b2cf8f3970af42a3dbd427a
Unknown <wieczorek.karolina1@o2.pl>
2018-09-23 23:46:01 +0200

    form component created

--- react/toDoList/src/Components/ToDoList.js
+++ react/toDoList/src/Components/ToDoList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ToDoItem from './ToDoList/ToDoItem.js'
+import AddItem from './ToDoList/AddItem.js'
 
 
 class ToDoList extends React.Component {
@@ -7,6 +8,7 @@ class ToDoList extends React.Component {
         super(props);
         this.state = {todos: ['wash up', 'eat some cheese', 'take a nap']}
         this.onDelete = this.onDelete.bind(this);
+        this.onAdd = this.onAdd.bind(this);
     }
 
     tick() {
@@ -16,7 +18,16 @@ class ToDoList extends React.Component {
           });    
         }.bind(this), 5000)
         
-    }    
+    }
+    
+    onAdd(item) {
+        var updatedTodos = this.state.todos;
+        updatedTodos.push(item);
+        
+        this.setState({
+            todos: updatedTodos
+       })   
+    }
     
     onDelete(item){
        var updatedTodos = this.state.todos.filter(function(val, index){
@@ -35,14 +46,15 @@ class ToDoList extends React.Component {
                 <main>
                     <div className="todo-list">
                         <h1>To Do List</h1>
-
                         <br />
-                        <ul style={{width: 500 + "px", margin: 'auto'}}>
+                        <ul style={{width: 500 + "px", margin: 'auto'}}>                       
                         {this.state.todos.map((item, index) => (
                             <ToDoItem item={item} key={index} onDelete={this.onDelete}/>
                         ))}
                         </ul>
-                        </div>    
+                        <br />
+                        <AddItem onAdd={this.onAdd}/>
+                       </div>               
                 </main>
             </div>
         )    
